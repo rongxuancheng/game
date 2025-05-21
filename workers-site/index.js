6f04fb00e3f91185817b99702c0a46d91fc8f505
rongxuancheng <atrong2022@outlook.com>
2025-05-21 21:09:20 +0800

    Update index.js

--- workers-site/index.js
+++ workers-site/index.js
@@ -1,5 +1,12 @@
-<script>
-  if (location.pathname === "/") {
-    location.replace("/index.html");
+import { getAssetFromKV } from '@cloudflare/kv-asset-handler';
+
+export default {
+  async fetch(request, env, ctx) {
+    try {
+      return await getAssetFromKV({ request, env });
+    } catch (err) {
+      // 如果找不到资源，就返回自定义 404 页面或默认错误
+      return new Response('Not Found', { status: 404 });
+    }
   }
-</script>
+}
